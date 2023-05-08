a6ee7608ece1f72ca8b8895d6245a24273d36f6c
10ge6 <dsf@jugemu.systems>
2023-05-08 17:45:06 -0300

    Update imeflex.sh

--- imeflex.sh
+++ imeflex.sh
@@ -8,7 +8,7 @@ gunzip flex-2.6.4.tar.gz
 tar -xf flex-2.6.4.tar
 
 cd flex-2.6.4
-./configure --prefix=$(realpath ./) && make && make install
+./configure --prefix=$(pwd) && make && make install
 
 cd ..
 wget http://gnu.c3sl.ufpr.br/ftp/bison/bison-3.8.2.tar.gz
@@ -16,9 +16,9 @@ gunzip bison-3.8.2.tar.gz
 tar -xf bison-3.8.2.tar
 
 cd bison-3.8.2
-./configure && make
+./configure --prefix=$(pwd) && make
 
-echo "export PATH=\"\$PATH:$(realpath ./src):$(realpath ../flex-2.6.4/bin)\"" >> ~/.bashrc
+echo "export PATH=\"\$PATH:$(pwd):$(realpath ../flex-2.6.4/bin)\"" >> ~/.bashrc
 cd ..
 
 rm -r flex-2.6.4.* bison-3.8.2.*
